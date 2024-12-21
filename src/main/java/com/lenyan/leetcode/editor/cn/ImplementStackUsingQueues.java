49d67bff0f402de342fea7bf046d6b2ee960c26d
lenyanjgk <128200330+lenyanjgk@users.noreply.github.com>
2024-12-21 10:58:32 +0800

    //225 用队列实现栈 implement-stack-using-queues //2024-12-21 10:57:44

---
+++ src/main/java/com/lenyan/leetcode/editor/cn/ImplementStackUsingQueues.java
@@ -0,0 +1,124 @@
+//225 用队列实现栈 implement-stack-using-queues
+//2024-12-21 10:57:44
+
+package com.lenyan.leetcode.editor.cn;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ImplementStackUsingQueues{
+    public static void main(String[] args) {
+        Solution solution = new ImplementStackUsingQueues().new Solution();
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class MyStack {
+
+        Queue<Integer> queue1; // 和栈中保持一样元素的队列
+        Queue<Integer> queue2; // 辅助队列
+
+        /** Initialize your data structure here. */
+        public MyStack() {
+            queue1 = new LinkedList<>();
+            queue2 = new LinkedList<>();
+        }
+
+        /** Push element x onto stack. */
+        public void push(int x) {
+            queue2.offer(x); // 先放在辅助队列中
+            while (!queue1.isEmpty()){
+                queue2.offer(queue1.poll());
+            }
+            Queue<Integer> queueTemp;
+            queueTemp = queue1;
+            queue1 = queue2;
+            queue2 = queueTemp; // 最后交换queue1和queue2，将元素都放到queue1中
+        }
+
+        /** Removes the element on top of the stack and returns that element. */
+        public int pop() {
+            return queue1.poll(); // 因为queue1中的元素和栈中的保持一致，所以这个和下面两个的操作只看queue1即可
+        }
+
+        /** Get the top element. */
+        public int top() {
+            return queue1.peek();
+        }
+
+        /** Returns whether the stack is empty. */
+        public boolean empty() {
+            return queue1.isEmpty();
+        }
+    }
+
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+}
+
+
+////请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。 
+//
+// 实现 MyStack 类： 
+//
+// 
+// void push(int x) 将元素 x 压入栈顶。 
+// int pop() 移除并返回栈顶元素。 
+// int top() 返回栈顶元素。 
+// boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。 
+// 
+//
+// 
+//
+// 注意： 
+//
+// 
+// 你只能使用队列的标准操作 —— 也就是 push to back、peek/pop from front、size 和 is empty 这些操作。 
+// 你所使用的语言也许不支持队列。 你可以使用 list （列表）或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。 
+// 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入：
+//["MyStack", "push", "push", "top", "pop", "empty"]
+//[[], [1], [2], [], [], []]
+//输出：
+//[null, null, null, 2, 2, false]
+//
+//解释：
+//MyStack myStack = new MyStack();
+//myStack.push(1);
+//myStack.push(2);
+//myStack.top(); // 返回 2
+//myStack.pop(); // 返回 2
+//myStack.empty(); // 返回 False
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= x <= 9 
+// 最多调用100 次 push、pop、top 和 empty 
+// 每次调用 pop 和 top 都保证栈不为空 
+// 
+//
+// 
+//
+// 进阶：你能否仅用一个队列来实现栈。 
+//
+// Related Topics 栈 设计 队列 👍 926 👎 0

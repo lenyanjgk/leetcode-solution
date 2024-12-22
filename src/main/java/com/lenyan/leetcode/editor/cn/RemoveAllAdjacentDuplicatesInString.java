04c45ff5fe7909448df3ae0ef83bc511bc3e89f1
lenyanjgk <128200330+lenyanjgk@users.noreply.github.com>
2024-12-22 11:16:24 +0800

    //150 逆波兰表达式求值 evaluate-reverse-polish-notation //2024-12-22 11:15:25

--- src/main/java/com/lenyan/leetcode/editor/cn/RemoveAllAdjacentDuplicatesInString.java
+++ src/main/java/com/lenyan/leetcode/editor/cn/RemoveAllAdjacentDuplicatesInString.java
@@ -8,7 +8,7 @@ public class RemoveAllAdjacentDuplicatesInString{
     public static void main(String[] args) {
         Solution solution = new RemoveAllAdjacentDuplicatesInString().new Solution();
     }
-    
+
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String removeDuplicates(String s) {
@@ -27,7 +27,28 @@ class Solution {
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
-
+    //队列思想做
+/*class Solution {
+    public String removeDuplicates(String S) {
+        //ArrayDeque会比LinkedList在除了删除元素这一点外会快一点
+        ArrayDeque<Character> deque = new ArrayDeque<>();
+        char ch;
+        for (int i = 0; i < S.length(); i++) {
+            ch = S.charAt(i);
+            if (deque.isEmpty() || deque.peek() != ch) {
+                deque.push(ch);
+            } else {
+                deque.pop();
+            }
+        }
+        String str = "";
+        //剩余的元素即为不重复的元素
+        while (!deque.isEmpty()) {
+            str = deque.pop() + str;
+        }
+        return str;
+    }
+}*/
     
 }
 

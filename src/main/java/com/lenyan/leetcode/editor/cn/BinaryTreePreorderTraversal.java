e1bf13757c9b15cff4bd4978572e98de2cddc84f
lenyanjgk <128200330+lenyanjgk@users.noreply.github.com>
2024-12-26 13:02:48 +0800

    //144 二叉树的前序遍历 binary-tree-preorder-traversal //2024-12-26 13:00:07

---
+++ src/main/java/com/lenyan/leetcode/editor/cn/BinaryTreePreorderTraversal.java
@@ -0,0 +1,112 @@
+//144 二叉树的前序遍历 binary-tree-preorder-traversal
+//2024-12-26 13:00:07
+
+package com.lenyan.leetcode.editor.cn;
+
+
+import com.lenyan.leetcode.editor.cn.model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePreorderTraversal{
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreePreorderTraversal().new Solution();
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        preorder(root, result);
+        return result;
+    }
+
+    public void preorder(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        result.add(root.val);
+        preorder(root.left, result);
+        preorder(root.right, result);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+}
+
+
+////给你二叉树的根节点 root ，返回它节点值的 前序 遍历。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+// 输入：root = [1,null,2,3] 
+// 
+//
+// 输出：[1,2,3] 
+//
+// 解释： 
+//
+// 
+//
+// 示例 2： 
+//
+// 
+// 输入：root = [1,2,3,4,5,null,8,null,null,6,7,9] 
+// 
+//
+// 输出：[1,2,4,5,6,7,3,8,9] 
+//
+// 解释： 
+//
+// 
+//
+// 示例 3： 
+//
+// 
+// 输入：root = [] 
+// 
+//
+// 输出：[] 
+//
+// 示例 4： 
+//
+// 
+// 输入：root = [1] 
+// 
+//
+// 输出：[1] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶：递归算法很简单，你可以通过迭代算法完成吗？ 
+//
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 1303 👎 0

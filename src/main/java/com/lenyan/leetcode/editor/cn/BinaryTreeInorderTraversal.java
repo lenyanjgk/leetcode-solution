e229099e0f3c9680481e43af47e9afdd9bebfc0a
lenyanjgk <128200330+lenyanjgk@users.noreply.github.com>
2024-12-26 13:07:37 +0800

    //94 二叉树的中序遍历

--- src/main/java/com/lenyan/leetcode/editor/cn/BinaryTreeInorderTraversal.java
+++ src/main/java/com/lenyan/leetcode/editor/cn/BinaryTreeInorderTraversal.java
@@ -46,4 +46,21 @@ class Solution {
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
+    class Solution1 {
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            dfs(root, res);
+            return res;
+        }
+
+        public void dfs(TreeNode root,List<Integer> res) {
+            if(root == null) {
+                return ;
+            }
+            dfs(root.left, res);
+            res.add(root.val);
+            dfs(root.right, res);
+        }
+    }
+
   }

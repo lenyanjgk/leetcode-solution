825d5c9a419ca352c6ebed98ce9a0a8a169f7975
lenyanjgk <128200330+lenyanjgk@users.noreply.github.com>
2024-12-31 10:16:57 +0800

    //102 二叉树的层序遍历 //2024年12月31日10点16分

--- src/main/java/com/lenyan/leetcode/editor/cn/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/com/lenyan/leetcode/editor/cn/BinaryTreeLevelOrderTraversal.java
@@ -1,5 +1,5 @@
 //102 二叉树的层序遍历
-//2024-10-03 22:47:12
+//2024年12月31日10点16分
 
   package com.lenyan.leetcode.editor.cn;
 
@@ -30,7 +30,60 @@ public class BinaryTreeLevelOrderTraversal{
  *     }
  * }
  */
+// 102.二叉树的层序遍历
 class Solution {
+    public List<List<Integer>> resList = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        //checkFun01(root,0);
+        checkFun02(root);
+
+        return resList;
+    }
+
+    //BFS--递归方式
+    public void checkFun01(TreeNode node, Integer deep) {
+        if (node == null) return;
+        deep++;
+
+        if (resList.size() < deep) {
+            //当层级增加时，list的Item也增加，利用list的索引值进行层级界定
+            List<Integer> item = new ArrayList<Integer>();
+            resList.add(item);
+        }
+        resList.get(deep - 1).add(node.val);
+
+        checkFun01(node.left, deep);
+        checkFun01(node.right, deep);
+    }
+
+    //BFS--迭代方式--借助队列
+    public void checkFun02(TreeNode node) {
+        if (node == null) return;
+        Queue<TreeNode> que = new LinkedList<TreeNode>();
+        que.offer(node);
+
+        while (!que.isEmpty()) {
+            List<Integer> itemList = new ArrayList<Integer>();
+            int len = que.size();
+
+            while (len > 0) {
+                TreeNode tmpNode = que.poll();
+                itemList.add(tmpNode.val);
+
+                if (tmpNode.left != null) que.offer(tmpNode.left);
+                if (tmpNode.right != null) que.offer(tmpNode.right);
+                len--;
+            }
+
+            resList.add(itemList);
+        }
+
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+class Solution1 {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> ans =  new ArrayList<List<Integer>>();
         if(root == null) {
@@ -56,6 +109,4 @@ class Solution {
         return ans;
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
-
   }

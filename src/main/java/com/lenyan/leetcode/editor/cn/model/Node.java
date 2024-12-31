fae4a9f322767fad3794f877662feb77dd3915a5
lenyanjgk <128200330+lenyanjgk@users.noreply.github.com>
2024-12-31 11:09:04 +0800

    //429 N 叉树的层序遍历 n-ary-tree-level-order-traversal //2024-12-31 11:05:13

--- src/main/java/com/lenyan/leetcode/editor/cn/model/Node.java
+++ src/main/java/com/lenyan/leetcode/editor/cn/model/Node.java
@@ -1,13 +1,33 @@
 package com.lenyan.leetcode.editor.cn.model;
 
+import java.util.List;
+
 public class Node {
     public int val;
     public Node next;
     public Node random;
+    public List<Node> children;
+
+    public Node() {}
 
     public Node(int val) {
         this.val = val;
         this.next = null;
         this.random = null;
+        this.children = null;
+    }
+
+    public Node(int val, Node next, Node random) {
+        this.val = val;
+        this.next = next;
+        this.random = random;
+        this.children = null;
+    }
+
+    public Node(int val, List<Node> children) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+        this.children = children;
     }
 }
